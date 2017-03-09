4ff3c9dba27e26d587646f69d17a03f8ddbc7c65
Henry Mercer <henry.mercer@me.com>
2017-03-09 19:01:56 +0000

    Fix routing for about page (#24)
    * Add routing to web.config
    
    * Fix typeof bug in analytics

--- src/client/app/shared/piwik/piwik.service.ts
+++ src/client/app/shared/piwik/piwik.service.ts
@@ -2,7 +2,7 @@ declare var _paq: Array<any>;
 
 export class PiwikTracker {
   public static TrackEvent(eventName: string, ...additionalInfo: any[]) {
-    if (_paq) {
+    if (typeof _paq !== 'undefined' && _paq) {
       _paq.push(['trackEvent', eventName].concat(additionalInfo));
     }
   }
